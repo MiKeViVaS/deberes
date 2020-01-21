93c2176a4809ab52d8031c55da9a13aa4eed4386
MiKe ViVaS <miguelvivasdavid@gmail.com>
2020-01-21 18:21:55 +0100

    deberesp1

---
+++ exercise2/index2.js
@@ -0,0 +1,8 @@
+let n1 = 3;
+let n2 = 7;
+
+function exercise2(_n1, _n2) {
+    let resultado = math.max(3,7);
+    return resultado;
+    
+}
---
+++ exercises3/index.js
@@ -0,0 +1,11 @@
+let n1 = 3;
+let n2 = 6;
+let n3 = 2;
+let n4 = 8;
+let n5 = 9;
+
+function exercise3(_n1, _n2,_n3,_n4,_n5) {
+   let resultado = Math.max(3,6,2,8,9);
+   return resultado;
+} 
+    
