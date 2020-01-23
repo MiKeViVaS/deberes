ed1f689072cfb2b283d9b3e9a9dca7c4a07ae7e7
Miguel Vivas David <mike_macbook@MacBook-Pro-de-Miguel.local>
2020-01-23 19:02:28 +0100

    exercises 11

---
+++ exercises11/index.js
@@ -0,0 +1,11 @@
+
+function rellenarArray (array1, array2){
+    var arrayVacio = []
+    console.log(arrayVacio)
+    return arrayVacio.push(array1 + " " + array2)
+    
+}
+
+
+var envioArray = rellenarArray([ "pera", "melocoton"],[ "sandia ","piña"]); 
+// document.write(envioArray);
---
+++ exercises11/index2.js
@@ -0,0 +1,15 @@
+function fusionArray(array1, array2) {
+    var array = []
+    console.log(array)
+    return array.push(array1 + " " + array2)
+}
+
+var unionArray = fusionArray([1,2,3,],[4,5,6]);
+
+function fusionArray(array1, array2) {
+    var array = []
+    console.log(array)
+    return array.push(array1 + " " + array2)
+}
+
+var unionArray = fusionArray(["hola","mundo"],[4,5,6]);
